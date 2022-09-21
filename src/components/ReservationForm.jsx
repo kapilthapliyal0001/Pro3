6dcbd3ab5c095c7dcfd762a4c498e1de4510d766
Kapil Thapliyal <kapilthapliyal0001@gmail.com>
2022-09-21 15:03:41 +0200

    added ReservationForm and Reservation component

--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -21,16 +21,13 @@ class ReservationForm extends Component {
     console.log(this.state.reservation);
 
     try {
-      let res = await fetch(
-        "https://striveschool.herokuapp.com/api/reservation",
-        {
-          method: "POST",
-          body: JSON.stringify(this.state.reservation),
-          headers: {
-            "Content-type": "application/json",
-          },
-        }
-      );
+      let res = await fetch("http://localhost:3001/reservations/", {
+        method: "POST",
+        body: JSON.stringify(this.state.reservation),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
       console.log(res.ok);
 
       if (res.ok) {
@@ -70,92 +67,95 @@ class ReservationForm extends Component {
   };
   render() {
     return (
-      <Form onSubmit={(e) => this.submitReservation(e)}>
-        <Form.Group className="mb-3">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="name"
-            placeholder="Name"
-            value={this.state.reservation.name}
-            onChange={(e) =>
-              this.setState({
-                reservation: {
-                  ...this.state.reservation,
-                  name: e.target.value,
-                },
-              })
-            }
-          />
-        </Form.Group>
+      <>
+        <h2>Reservation Forum</h2>
+        <Form onSubmit={(e) => this.submitReservation(e)}>
+          <Form.Group className="mb-3">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Name"
+              value={this.state.reservation.name}
+              onChange={(e) =>
+                this.setState({
+                  reservation: {
+                    ...this.state.reservation,
+                    name: e.target.value,
+                  },
+                })
+              }
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Phone</Form.Label>
-          <Form.Control
-            type="phone"
-            placeholder="Enter Contact phone"
-            value={this.state.reservation.phone}
-            onChange={(e) => {
-              this.setState({
-                reservation: {
-                  ...this.state.reservation,
-                  phone: e.target.value,
-                },
-              });
-            }}
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Number of persons</Form.Label>
-          <Form.Control
-            as="select"
-            onChange={(e) => this.inputChange(e)}
-            id="persons"
-            value={this.state.persons}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">Table of 4</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Check
-            type="checkbox"
-            label="Do you smoke?"
-            checked={this.state.reservation.smoking}
-            id="smoking"
-            onChange={(e) => this.inputChange(e)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Date & Time</Form.Label>
-          <Form.Control
-            type="datetime-local"
-            value={this.state.reservation.date_time}
-            onChange={(e) =>
-              this.setState({
-                reservation: {
-                  ...this.state.reservation,
-                  date_time: e.target.value,
-                },
-              })
-            }
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Any special request?</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={3}
-            value={this.state.reservation.specialRequests}
-            id="specialRequests"
-            onChange={this.inputChange}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control
+              type="phone"
+              placeholder="Enter Contact phone"
+              value={this.state.reservation.phone}
+              onChange={(e) => {
+                this.setState({
+                  reservation: {
+                    ...this.state.reservation,
+                    phone: e.target.value,
+                  },
+                });
+              }}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Number of persons</Form.Label>
+            <Form.Control
+              as="select"
+              onChange={(e) => this.inputChange(e)}
+              id="persons"
+              value={this.state.persons}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">Table of 4</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Check
+              type="checkbox"
+              label="Do you smoke?"
+              checked={this.state.reservation.smoking}
+              id="smoking"
+              onChange={(e) => this.inputChange(e)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Date & Time</Form.Label>
+            <Form.Control
+              type="datetime-local"
+              value={this.state.reservation.date_time}
+              onChange={(e) =>
+                this.setState({
+                  reservation: {
+                    ...this.state.reservation,
+                    date_time: e.target.value,
+                  },
+                })
+              }
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Any special request?</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={this.state.reservation.specialRequests}
+              id="specialRequests"
+              onChange={this.inputChange}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </>
     );
   }
 }
--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,9 +1,41 @@
 /** @format */
 
 import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 export default class Reservations extends Component {
+  state = {
+    reservations: [], //empty state
+  };
+
+  componentDidMount = async () => {
+    // component did mount is fired whenever there
+    // this place is perfect for the fetches
+    const response = await fetch("http://localhost:3001/reservations/");
+    let data = await response.json();
+    console.log(data);
+    this.setState({
+      reservations: data,
+    });
+  };
+
   render() {
-    return <h1>Let's fetch them</h1>;
+    // render is going to be executed whenever there is a change in the state or the props
+    // never set state in the render
+    console.log("print from the render");
+    return (
+      <>
+        {/* This is a react fragment to wrap things up */}
+        <ListGroup>
+          {this.state.reservations.length === 0 ? (
+            <h3>There is no such reservations </h3>
+          ) : (
+            this.state.reservations.map((res) => (
+              <ListGroupItem>{res.name}</ListGroupItem>
+            ))
+          )}
+        </ListGroup>
+      </>
+    );
   }
 }
