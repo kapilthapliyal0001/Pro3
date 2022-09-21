69853b1a3142afc0964d4d5c68cbf168ba71e72d
Kapil Thapliyal <kapilthapliyal0001@gmail.com>
2022-09-21 16:04:19 +0200

    added Error and loader component

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from "react";
+import { Alert } from "react-bootstrap";
+
+export default function Error() {
+  return (
+    <Alert variant="danger">
+      <h2>There is some error </h2>
+    </Alert>
+  );
+}
--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -3,20 +3,36 @@
 import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 
+// importing component
+import Loader from "./Loader";
+import Error from "./Error";
 export default class Reservations extends Component {
   state = {
     reservations: [], //empty state
+    isLoading: true,
+    isError: false,
   };
 
   componentDidMount = async () => {
     // component did mount is fired whenever there
     // this place is perfect for the fetches
-    const response = await fetch("http://localhost:3001/reservations/");
-    let data = await response.json();
-    console.log(data);
-    this.setState({
-      reservations: data,
-    });
+
+    try {
+      const response = await fetch("http://localhost:3001/reservations/");
+      let data = await response.json();
+      console.log(data);
+      this.setState({
+        reservations: data,
+        isLoading: false,
+      });
+    } catch (error) {
+      console.log(error, " :following is the error");
+      this.setState({
+        ...this.state,
+        isLoading: false,
+        isError: true,
+      });
+    }
   };
 
   render() {
@@ -25,16 +41,21 @@ export default class Reservations extends Component {
     console.log("print from the render");
     return (
       <>
-        {/* This is a react fragment to wrap things up */}
-        <ListGroup>
-          {this.state.reservations.length === 0 ? (
-            <h3>There is no such reservations </h3>
-          ) : (
-            this.state.reservations.map((res) => (
-              <ListGroupItem>{res.name}</ListGroupItem>
-            ))
-          )}
-        </ListGroup>
+        {/* and operator need to be true for both the conditions and the jsx is always true so, the first statement is the cond. */}
+        {this.state.isLoading && <Loader />}
+        {this.state.isError && <Error />}
+
+        {this.state.reservations.length === 0 &&
+        this.state.isLoading === false &&
+        this.state.isError === false ? (
+          <p>There is no reservations </p>
+        ) : (
+          <ListGroup>
+            {this.state.reservations.map((res) => (
+              <ListGroup.Item>{res.name}</ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
       </>
     );
   }
